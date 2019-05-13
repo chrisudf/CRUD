c101ed7b87b53f513442f34b8af4f0b8a60b6eb3
DESKTOP-OFH8I45\Qiu <chengqiu03@gmail.com>
2019-05-13 11:18:01 +1000

    extract logic

---
+++ src/routes/posts.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const {
+  getAllPost,
+  getPostById,
+  updatePostById,
+  addPost,
+  deletePostById
+} = require('../controllers/posts');
+
+const validateId = require('../middleware/validatedId');
+
+const router = express.Router();
+
+router.get('', getAllPost);
+
+router.get('/:id', validateId, getPostById);
+ 
+router.post('', addPost);
+
+router.put('/:id', validateId, updatePostById);
+
+router.delete('/:id', validateId, deletePostById);
+
+module.exports = router;
