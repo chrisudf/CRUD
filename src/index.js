96a4c1c1654a7c470cfde7d71731a676dcc42269
DESKTOP-OFH8I45\Qiu <chengqiu03@gmail.com>
2019-05-13 11:45:19 +1000

    add helmet,morganlong,cor:

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 const express = require ('express')
+require('dotenv').config()
 const routes =require('./routes')
+const helmet = require('helmet');
+const morgan = require('morgan');
+const cors = require('cors');
 const app = express()
 
+const PORT =process.env.PORT || 8888
+
+
+const morganLog =
+  process.env.NODE_ENV === 'production' ? morgan('common') : morgan('dev');
+
 app.use(express.json())
+app.use(helmet());
+app.use(morganLog);
+app.use(cors());
 app.use('/v1',routes)
 
-app.listen(8888,()=>{
-    console.log('server listening to port 8888')
+app.listen(PORT,()=>{
+    console.log(`server listening to port ${PORT}`)
 })
